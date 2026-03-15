c234c729f9f509c9ad8a492d4fa7e1314217dc7b
Adam Ghose <adamghose@gmail.com>
2026-03-15 19:21:49 -0400

    Revert "added timer"
    This reverts commit 65f03825c4c04cc1101694c09f79a214fcba0fb9.

--- brain-boost/Scripts/wordle_like.gd
+++ brain-boost/Scripts/wordle_like.gd
@@ -3,22 +3,16 @@ extends Control
 @onready var text: RichTextLabel = $ColorRect/RichTextLabel2
 @onready var inputText: LineEdit = $ColorRect/LineEdit
 @onready var scoreOutput: RichTextLabel = $ColorRect/Score
-@onready var timeText: RichTextLabel = $ColorRect/RichTextLabel
-@onready var popUp: ColorRect = $GameEndPopUp
-@onready var finalScore: RichTextLabel = $"GameEndPopUp/Final Score"
+@onready var extraText: RichTextLabel = $ColorRect/RichTextLabel
 
-@export var countdown_time: float = 300
-
-var time
 var words
 var answer
-var score = 0
-var new_word_needed
-var countdownActive
+var score = 0;
+var new_word_needed = true;
 
 func read_text(file_path) -> void:
 # vector of words
-	words = []
+	words = [];
 	var file = FileAccess.open(file_path, FileAccess.READ)
 	if file:
 		while not file.eof_reached():
@@ -36,51 +30,29 @@ func read_text(file_path) -> void:
 func _ready() -> void:
 	scoreOutput.text = str(score)
 	read_text("res://sgb-words.txt")
-	time = countdown_time
-	countdownActive = true
-	new_word_needed = true
-	popUp.hide()
+	
+	
 	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	#check if time > 0
-	if (int(time) % 60) > 0 or floor(time /60) > 0:
-		time -= delta
-		var mins = floor(time /60)
-		var secs = int(time) % 60
-		timeText.text = "%1d:%02d" % [mins, secs]
-	else:
-		#end game
-		time = 0
-		countdownActive = false;
-		inputText.text = ""
-		timeText.text = "0:00"
-		popUp.show()
-		finalScore.text = "Final Score: " + str(score)
-	
+func _process(_delta: float) -> void:
 	if new_word_needed:
 		answer = words[randi() % words.size()]
 		
 		#scramble answer!!
 		var scram_answer = answer
 		for i in range(1):
-			var index = (randi() % (answer.length() - 1)) + 1
+			var index = randi() % answer.length()
 			var scram1 = answer.substr(0, index) 
 			var scram2 = answer.substr(index, answer.length())
 			scram_answer = scram2 + scram1
 		text.text = scram_answer
-		print(answer)
+		extraText.text = answer
 		new_word_needed = false
 	else:
-		#check if input is answer
 		if inputText.text == answer:
 			new_word_needed = true
 			score = score + 1
 			scoreOutput.text = str(score)
 			inputText.text = ""
-
-
-func _on_exit_button_button_up() -> void:
-	get_tree().change_scene_to_file("res://home_menu.tscn")
