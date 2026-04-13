38d6aae1d1022e8554dbcd1541430b750ad60838
Caden Rogers <phurpel@gmail.com>
2026-04-13 11:38:21 -0400

    post win functionality
    improved sudoku visuals and made smoother to replay over and over

--- brain-boost/Scripts/game_sudoku.gd
+++ brain-boost/Scripts/game_sudoku.gd
@@ -16,15 +16,7 @@ var boardData = []
 #0,4,0,0,5,0,0,3,6,
 #7,0,3,0,1,8,0,0,0
 #------------------
-#4,3,5,2,6,9,7,8,1,
-#6,8,2,5,7,1,4,9,3,
-#1,9,7,8,3,4,5,6,2,
-#8,2,6,1,9,5,3,4,7,
-#3,7,4,6,8,2,9,1,5,
-#9,5,1,7,4,3,6,2,8,
-#5,1,9,3,2,6,8,7,4,
-#2,4,8,9,5,7,1,3,6,
-#7,6,3,4,1,8,2,5,0];
+#4,3,5,2,6,9,7,8,1,6,8,2,5,7,1,4,9,3,1,9,7,8,3,4,5,6,2,8,2,6,1,9,5,3,4,7,3,7,4,6,8,2,9,1,5,9,5,1,7,4,3,6,2,8,5,1,9,3,2,6,8,7,4,2,4,8,9,5,7,1,3,6,7,6,3,4,1,8,2,5,0];
 	
 var startBoards = [
 	[8,0,1,0,0,3,9,0,6,0,0,9,0,0,7,8,5,0,2,5,0,1,0,0,4,7,0,5,0,0,0,6,1,7,0,4,7,6,0,8,3,0,0,0,0,0,3,2,0,0,0,0,0,0,0,2,0,0,1,9,5,0,0,0,0,5,0,0,0,3,0,2,0,0,0,4,5,2,1,9,7],
@@ -46,7 +38,7 @@ func _ready() -> void:
 #
 func createBoard():
 	#sb = startBoards random index of starting board
-	var sb = randi_range(0,5);
+	var sb = randi_range(0,5)
 	for i in range(81):
 		var btn = Button.new();
 		if(startBoards[sb][i] != 0):
@@ -86,7 +78,10 @@ func _process(delta: float) -> void:
 	
 	if(checkWin()):
 		$Victory.text = "Winner"
-		#$ColorRect.color = Color.GREEN
+		$Timer.paused = true
+		$ReplayButton.show()
+		$ReplayButton.text = "Replay"
+		
 
 
 func handleInput(number):
